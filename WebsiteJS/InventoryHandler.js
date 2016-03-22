f6029779a14db6f11a933fd08a33e1d2229f487a
mindcoding <slimercraft3@gmail.com>
2016-03-22 10:27:51 -0400

    Update InventoryHandler.js

--- WebsiteJS/InventoryHandler.js
+++ WebsiteJS/InventoryHandler.js
@@ -1,5 +1,7 @@
 var SteamApi = require('steam-api');
 
+var market = require('steam-market-pricing');
+
 var items = new SteamApi.Items('steam-api-key', optionalSteamId);
 
 Var Inv;
@@ -11,4 +13,11 @@ items.GetPlayerItems(730, 76561198125081821).done(function(result){
   Inv = result;
 
 });
+Var names = inv;
 
+market.getItemsPrice(730, names, function(data) {
+    //console.log(data); 
+    for(var i in names) {
+        console.log(names[i] + ' median price: ' + data[names[i]]['median_price']);
+    }
+});
